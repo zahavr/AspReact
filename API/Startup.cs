3b66a685390640e4bf8df81a441a235a032d50d4
zahavr <leshabiceps34@gmail.com>
2021-07-16 18:59:20 +0600

    Create client app. Pre-settings for CORS. Install semantic and axios .

--- API/Startup.cs
+++ API/Startup.cs
@@ -10,46 +10,57 @@ using Persistence;
 namespace API
 {
 	public class Startup
-    {
+	{
 		private readonly IConfiguration _configuration;
 
 		public Startup(IConfiguration configuration)
-        {
+		{
 			_configuration = configuration;
 		}
 
-        public void ConfigureServices(IServiceCollection services)
-        {
+		public void ConfigureServices(IServiceCollection services)
+		{
 
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
-            services.AddDbContext<DataContext>(opt => { 
-                opt.UseSqlite(_configuration.GetConnectionString("DefaultConnection"));
-            });
-        }
+			services.AddControllers();
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+			});
+			services.AddDbContext<DataContext>(opt =>
+			{
+				opt.UseSqlite(_configuration.GetConnectionString("DefaultConnection"));
+			});
+			services.AddCors(opt =>
+			{
+				opt.AddPolicy("CorsPolicy", policy =>
+				{
+					policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+				});
+			});
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-            }
+		}
 
-            // app.UseHttpsRedirection();
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+				app.UseSwagger();
+				app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+			}
 
-            app.UseRouting();
+			// app.UseHttpsRedirection();
 
-            app.UseAuthorization();
+			app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
-    }
+			app.UseCors("CorsPolicy");
+
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
+		}
+	}
 }
