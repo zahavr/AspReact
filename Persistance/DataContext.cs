a03b130bbffa54808c0dca5d5c826da15c0d87c2
zahavr <leshabiceps34@gmail.com>
2021-07-16 15:44:01 +0600

    Add project files.

---
+++ Persistance/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Persistance
+{
+	public class DataContext : DbContext
+    {
+		public DataContext([NotNull] DbContextOptions options) : base(options)
+		{
+		}
+
+		public DbSet<Activity> Activities { get; set; }
+	}
+}
