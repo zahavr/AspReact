29d34868c69bb2204a4ce2bc1341724f1067a837
zahavr <leshabiceps34@gmail.com>
2021-07-18 17:08:53 +0600

    Add CQRS and autoMapper. CRUD operation for activities

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,11 +1,9 @@
+using API.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-using Persistence;
 
 namespace API
 {
@@ -20,24 +18,7 @@ namespace API
 
 		public void ConfigureServices(IServiceCollection services)
 		{
-
-			services.AddControllers();
-			services.AddSwaggerGen(c =>
-			{
-				c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-			});
-			services.AddDbContext<DataContext>(opt =>
-			{
-				opt.UseSqlite(_configuration.GetConnectionString("DefaultConnection"));
-			});
-			services.AddCors(opt =>
-			{
-				opt.AddPolicy("CorsPolicy", policy =>
-				{
-					policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-				});
-			});
-
+			services.AddApplicationServices(_configuration);
 		}
 
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
---
+++ client-app/src/app/layout/NavBar.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Button, Container, Menu} from 'semantic-ui-react';
+
+interface Props {
+    openForm: () => void;
+}
+
+export default function NavBar({openForm}: Props) {
+    return (
+        <Menu inverted fixed={'top'}>
+            <Container>
+                <Menu.Item>
+                    <img src={'/assets/logo.png'} alt={'logo'} style={{marginRight: '10px'}}/>
+                    Reactivities
+                </Menu.Item>
+                <Menu.Item name={'Activities'}/>
+                <Menu.Item>
+                    <Button positive content={'Create Activity'} onClick={openForm}/>
+                </Menu.Item>
+            </Container>
+        </Menu>
+    )
+}
