20584b04a10a55ef00769526be864ce5e13cc0bf
zahavr <leshabiceps34@gmail.com>
2021-08-24 12:01:42 +0600

    Add form and input validation with formik

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'react-toastify/dist/ReactToastify.min.css';
 import 'semantic-ui-css/semantic.min.css'
 import 'react-calendar/dist/Calendar.css';
+import 'react-datepicker/dist/react-datepicker.css'
 import './app/layout/style.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
